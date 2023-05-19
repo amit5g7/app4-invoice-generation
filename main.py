eddf1a69538247a2d7b44086ff539d144f8f661a
Amit Gupta <amitguptag@gmail.com>
2023-05-19 15:30:29 +0530

    Final

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ filepaths = glob.glob("invoices/*.xlsx")
 for filepath in filepaths:
     df = pd.read_excel(filepath,sheet_name="Sheet 1")
     total_price_sum = df["total_price"].sum()
-    print(total_price_sum)
-    print(df)
+    #print(total_price_sum)
+    #print(df)
     pdf= FPDF(orientation="P", unit= "mm", format="A4")
     pdf.add_page()
     filename=Path(filepath).stem
@@ -34,10 +34,10 @@ for filepath in filepaths:
 
     for col in df.columns:
 
-        print(total_price_sum)
-        print(col)
+        #print(total_price_sum)
+        #print(col)
         if col == "total_price":
-            print(rows[col])
+            #print(rows[col])
             pdf.cell(w=38, h=5, txt=f"{total_price_sum}", border=1, align="C",)
         else:
             pdf.cell(w=38, h=5, border=1, align="C", )
@@ -50,4 +50,4 @@ for filepath in filepaths:
 
 
 
-    pdf.output(f"PDFs/{filename}.pdf")
+pdf.output(f"PDFs/{filename}.pdf")
